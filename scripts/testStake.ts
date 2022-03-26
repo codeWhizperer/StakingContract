148c5650309349a7859b962ac76149899cf6731f
Adegbite Ademola Kelvin <Adegbiteademola1999@gmail.com>
2022-03-26 17:01:28 +0100

    changes

--- scripts/testStake.ts
+++ scripts/testStake.ts
@@ -2,7 +2,7 @@
 import {ethers} from "hardhat";
 
 async function main(){
-  const deployedStakeAddress = "0x40a42Baf86Fc821f972Ad2aC878729063CeEF403";
+  const deployedStakeAddress = "0x4bf010f1b9beDA5450a8dD702ED602A104ff65EE";
   const BOREDAPEOWNER = "0x9e64b47bbdb9c1f7b599f11987b84c416c0c4110";
   const DEPLOYEDBAP20 = "0xEd4E1f3f9ad315288705713BF3105E2d9976Aa3a";
   const BOREDAPECONTRACT ="0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D";
@@ -15,7 +15,7 @@ async function main(){
   });
   const signer = await ethers.getSigner(BOREDAPEOWNER)
 
-// const approve = ERC20.connect(signer).approve(deployedStakeAddress,1000000)
+// const approve = ERC20.connect(signer).approve(deployedStakeAddress,2000000)
 // console.log(await (await approve).wait())
 
 // const allowance = await ERC20.connect(signer).allowance(BOREDAPEOWNER, deployedStakeAddress);
@@ -24,27 +24,25 @@ async function main(){
 
     // console.log("YOUR BALANCE BEFORE",await ERC20.balanceOf(BOREDAPEOWNER))
     
-    //  await deployedStake.connect(signer).stakeToken(1000)
+    //  await deployedStake.connect(signer).stakeToken(20000)
 
     //  console.log("YOUR BALANCE AFTER",await ERC20.balanceOf(BOREDAPEOWNER))
 
 
 
-// const Stake = await deployedStake.stakeToken(
-//   100
-// )
+
 // 0x4bf010f1b9beDA5450a8dD702ED602A104ff65EE
 
 
 // give allowance to contract
 // suppose the current block has a timestamp of 01:00 PM
 // @ts-ignore
-await network.provider.send("evm_increaseTime", [259200])
-// @ts-ignore
-await network.provider.send("evm_mine") // this one will have 02:00 PM as its timestamp
+// await network.provider.send("evm_increaseTime", [259200])
+//  @ts-ignore
+// await network.provider.send("evm_mine") // this one will have 02:00 PM as its timestamp
 
-await deployedStake.withdraw(300);
-console.log(await ERC20.balanceOf(signer.address))
+await deployedStake.withdraw(100);
+console.log(await ERC20.balanceOf(BOREDAPEOWNER))
 
 
 }
